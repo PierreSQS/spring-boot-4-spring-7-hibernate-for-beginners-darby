a62e80d884fdffba0289679a752b95d3046f8a91
PierreSQS <nomo_messina@web.de>
2025-12-28 04:41:43 +0100

    Sec4_Chap100-XX: added Lombok to Student entity

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-students-list-base/src/main/java/com/luv2code/demo/entity/Student.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-students-list-base/src/main/java/com/luv2code/demo/entity/Student.java
@@ -1,32 +1,20 @@
 package com.luv2code.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
 
     private String firstName;
     private String lastName;
 
-    public Student() {
 
-    }
-
-    public Student(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
