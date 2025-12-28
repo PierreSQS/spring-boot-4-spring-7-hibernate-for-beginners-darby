2299a1e47e872d7d466f92baf9269625a256acef
PierreSQS <nomo_messina@web.de>
2025-12-28 04:32:56 +0100

    Sec4_Chap100-XX: optimized test code

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-students-list-base/src/test/java/com/luv2code/demo/DemoApplicationTests.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-students-list-base/src/test/java/com/luv2code/demo/DemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.luv2code.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
